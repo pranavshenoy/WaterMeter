ca88a0acc6e6377a1b7124dd575c8cbde190db8c
Akhil Mohan <akhilerm@gmail.com>
2015-11-24 00:29:54 +0530

    Added text field and swicth to add_tank(layout only).

--- app/src/main/java/com/slateandpencil/watermeter/add_tank.java
+++ app/src/main/java/com/slateandpencil/watermeter/add_tank.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -19,15 +20,35 @@ public class add_tank extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        Toast.makeText(add_tank.this, "NEw activity", Toast.LENGTH_SHORT).show();
 
     }
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
-        overridePendingTransition(R.anim.abc_grow_fade_in_from_bottom, R.anim.abc_slide_out_bottom);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_actions, menu);
         return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent intent;
+        switch (item.getItemId()){
+            case R.id.save:
+                Toast.makeText(add_tank.this, "Added New Tank", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.delete:
+                Toast.makeText(add_tank.this, "Changes Discarded", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Toast.makeText(add_tank.this, "Changes Discarded", Toast.LENGTH_SHORT).show();
+
+        }
+       /* intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();*/
+        super.onBackPressed();
+        overridePendingTransition(R.anim.abc_grow_fade_in_from_bottom, R.anim.abc_slide_out_bottom);
+        return true;//return super.onOptionsSelected(MenuItem item);
+
 
     }
     @Override
